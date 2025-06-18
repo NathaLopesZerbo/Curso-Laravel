4d340f5c3470ee8ce19e5a4c64f8155d8d68ecaa
Nathã Lopes Zerbo <nathalopeszerbo7@gmail.com>
2025-06-18 11:44:51 -0300

    Alterando nome

---
+++ app/Http/Controllers/Site/SiteController.php
@@ -0,0 +1,19 @@
+<?php 
+
+    namespace App\Http\Controllers\Site;
+
+    class SiteController{
+        public function contact(){
+            return view('site/contact');
+        }
+    }
+
+
+
+
+
+
+
+
+
+?>
