8b8889ddefa874078ec9e116dafe9e73bf55dcf8
Nathã Lopes Zerbo <nathalopeszerbo7@gmail.com>
2025-06-18 15:16:30 -0300

    Inserindo services

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -5,19 +5,25 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUpdateSupport;
 use App\Models\Support;
+use App\Service\SupportService;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index(Support $support){
 
-        $supports = $support->all();
+    public function __construct(protected SupportService $service){
+
+    }
+
+    public function index(Request $request){
+
+        $supports = $this->service->getAll($request->filter);
 
         return view('admin/supports/index', compact('supports'));
     }
 
     public function show(string | int $id) {
-        if(!$support = Support::find($id)){
+        if(!$support = $this->service->findOne($id)){
             return back();
         }
         return view('admin/supports/show', compact('support'));
@@ -35,8 +41,9 @@ class SupportController extends Controller
         return redirect()->route('supports.index');
     }
 
-    public function edit( Support $support, string|int $id){
-        if(!$support = $support->where('id', $id)->first()){
+    public function edit( string $id){
+        // if(!$support = $support->where('id', $id)->first())
+        if(!$support = $this->service->findOne($id)){
             return back();
         }
 
@@ -52,11 +59,8 @@ class SupportController extends Controller
         return redirect()->route('supports.index');
     }
 
-    public function destroy(string | int $id){
-        if(!$support = Support::find($id)){ 
-            return back();
-        }
-        $support->delete();
+    public function destroy(string $id){
+        $this->service->delete($id);
 
         return redirect()->route('supports.index');
     }
