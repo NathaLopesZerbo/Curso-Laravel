e99aa4361e31d5364da09247e398b9cd02f9003a
Nathã Lopes Zerbo <nathalopeszerbo7@gmail.com>
2025-06-20 11:59:23 -0300

    Conversão de arrays

--- app/Repositories/PaginationInterface.php
+++ app/Repositories/PaginationInterface.php
@@ -2,7 +2,18 @@
 
 namespace App\Repositories;
 
+use stdClass;
 
 interface PaginationInterface{
-    
+
+   /**
+    * @return stdClass[]
+    */
+
+    public function items(): array;
+    public function total(): int;
+    public function isFirstPage(): bool;
+    public function currentPage(): int;
+    public function getNumberNextPage(): int;
+    public function getNumberPreviousPage(): int;
 }
