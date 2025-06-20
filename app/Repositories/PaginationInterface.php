b0a40e12f353f1447f739be4a67a114b79865fab
Nathã Lopes Zerbo <nathalopeszerbo7@gmail.com>
2025-06-20 14:36:37 -0300

    build(Inserindo diversas funcionalidades): Colocando filtros diferentes
    teste
    
    teste

--- app/Repositories/PaginationInterface.php
+++ app/Repositories/PaginationInterface.php
@@ -13,6 +13,7 @@ interface PaginationInterface{
     public function items(): array;
     public function total(): int;
     public function isFirstPage(): bool;
+    public function isLastPage(): bool;
     public function currentPage(): int;
     public function getNumberNextPage(): int;
     public function getNumberPreviousPage(): int;
